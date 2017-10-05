7a9ae229f8e7468275a7afc842ecdcef0f421de0
Ruslan Akberov <ruslanakberov@gmail.com>
2017-10-05 21:26:32 +0300

    Pod installed

---
+++ RainyShine/WeatherVC.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherVC.swift
+//  RainyShine
+//
+//  Created by Руслан Акберов on 05.10.2017.
+//  Copyright © 2017 Ruslan Akberov. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
